64baf666ac435968013a8f635e2ed72f8d0d5ada
kenta-s <knt01222@gmail.com>
2020-12-30 20:38:01 +0900

    routing

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+mod other {
+    #[get("/world")]
+    pub fn world() -> &'static str {
+        "Hello, world!"
+    }
+}
+
+#[get("/hello")]
+pub fn hello() -> &'static str {
+    "Hello, outside world!"
+}
+
+fn main() {
+    // /hello/hello
+    // /hello/world
+    rocket::ignite().mount("/hello", routes![hello, other::world]).launch();
+}
